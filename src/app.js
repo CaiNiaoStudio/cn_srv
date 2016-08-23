af16a55f420d6e5cbd058340b7e163bad9bbc409
陈俊生 <junsheng.chen@ggcar.com>
2016-08-23 16:11:23 +0800

    添加favicon

--- src/app.js
+++ src/app.js
@@ -4,83 +4,87 @@ import path from 'path'
 import views from 'koa-views'
 import convert from 'koa-convert'
 import json from 'koa-json'
+import favicon from 'koa-favicon'
 import Bodyparser from 'koa-bodyparser'
 import logger from 'koa-logger'
 import koaStatic from 'koa-static-plus'
 import koaOnError from 'koa-onerror'
 import config from './config'
 
-const app = new Koa()
-const bodyparser = Bodyparser()
+const app = new Koa();
+const bodyparser = Bodyparser();
 
 // middlewares
-app.use(convert(bodyparser))
-app.use(convert(json()))
-app.use(convert(logger()))
+app.use(convert(bodyparser));
+app.use(convert(json()));
+app.use(convert(logger()));
+
+// favicon
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 
 // static
 app.use(convert(koaStatic(path.join(__dirname, '../public'), {
   pathPrefix: ''
-})))
+})));
 
 // views
 app.use(views(path.join(__dirname, '../views'), {
   extension: 'ejs'
-}))
+}));
 
 // 500 error
 koaOnError(app, {
   template: 'views/500.ejs'
-})
+});
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // response router
 app.use(async (ctx, next) => {
-  await require('./routes').routes()(ctx, next)
-})
+  await require('./routes').routes()(ctx, next);
+});
 
 // 404
 app.use(async (ctx) => {
-  ctx.status = 404
-  await ctx.render('404')
-})
+  ctx.status = 404;
+  await ctx.render('404');
+});
 
 // error logger
 app.on('error', async (err, ctx) => {
-  console.log('error occured:', err)
-})
+  console.log('error occured:', err);
+});
 
-const port = parseInt(config.port || '3000')
-const server = http.createServer(app.callback())
+const port = parseInt(config.port || '3000');
+const server = http.createServer(app.callback());
 
-server.listen(port)
+server.listen(port);
 server.on('error', (error) => {
   if (error.syscall !== 'listen') {
-    throw error
+    throw error;
   }
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(port + ' requires elevated privileges')
-      process.exit(1)
-      break
+      console.error(port + ' requires elevated privileges');
+      process.exit(1);
+      break;
     case 'EADDRINUSE':
-      console.error(port + ' is already in use')
-      process.exit(1)
-      break
+      console.error(port + ' is already in use');
+      process.exit(1);
+      break;
     default:
-      throw error
+      throw error;
   }
-})
+});
 server.on('listening', () => {
-  console.log('Listening on port: %d', port)
-})
+  console.log('Listening on port: %d', port);
+});
 
-export default app
+export default app;
